2e7dcb7d26935665fc72fb9311317d87ab5ced68
kyu <kyu@172.30.1.1>
2017-08-18 10:06:43 +0900

    초기 설정 세팅

---
+++ src/main/java/com/kyunam/springkyunam/SpringKyunamApplication.java
@@ -0,0 +1,12 @@
+package com.kyunam.springkyunam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKyunamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKyunamApplication.class, args);
+	}
+}
